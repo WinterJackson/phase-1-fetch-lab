ad32d20519c3fb526d6d2893d78f7e31bd274a31
WinterJackson <winterjacksonwj@gmail.com>
2023-06-22 11:46:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => {
+      // Process the retrieved book data
+      renderBooks(data);
+      return data;
+    })
   
 }
 
